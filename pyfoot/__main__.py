78e64cc84dcd563babe063800c96c30a274de83e
Threated <jan2001.07@gmail.com>
2020-02-21 16:43:04 +0100

    Updatet CLI

--- pyfoot/__main__.py
+++ pyfoot/__main__.py
@@ -1,19 +1,23 @@
-
-
 def init_folders():
-    import argparse, os, pathlib, shutil
-    parser = argparse.ArgumentParser(prog="pyfoot", description=__doc__)
-    parser.add_argument("init")
-    parser.add_argument("name")
-    parser.add_argument("-dir", nargs="?", default=".")
+    import argparse
+    import os
+    import pathlib
+    import shutil
+    parser = argparse.ArgumentParser(prog="pyfoot", description="This is the pyfoot cli used for quickly creating the baseline structure of your Project.")
+    parser.add_argument("init", choices=["init"])
+    parser.add_argument("projectname", help="Name of the Project")
+    parser.add_argument("-dir", nargs="?", default=".", help="Can be specified to initialize the project in a given subdirectory")
     args = parser.parse_args()
     if args.init == "init":
-        os.chdir(args.dir)
-        os.mkdir(args.name)
-        os.chdir(args.name)
+        p = pathlib.Path(args.dir)
+        p.mkdir(parents=True, exist_ok=True)
+        os.chdir(p.as_posix())
+        os.mkdir(args.projectname)
+        os.chdir(args.projectname)
         os.mkdir("Graphics")
         os.mkdir("Sounds")
-        examplefile = (pathlib.Path(__file__).parent.parent/"Examples/example.py").as_posix()
+        examplefile = (pathlib.Path(__file__).parent.parent / "Examples/example.py").as_posix()
         shutil.copy2(examplefile, pathlib.Path(".").as_posix())
+
 
 init_folders()
